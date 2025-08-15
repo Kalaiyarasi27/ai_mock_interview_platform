ec7586481426e310e2b8b823693fc263adbb8a69
Kalaiyarasi_27 <kalaiyarasi385@gmail.com>
2025-08-15 23:58:33 +0530

    setup app,file and folder structure,routing and auth pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from '@/components/AuthForm'; // adjust path to match your folder structure
+
+const Page = () => {
+  return <AuthForm type="sign-in" />;
+};
+
+export default Page;
